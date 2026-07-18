fc7fcea2eed1c3626a6db5de19ebf1a200f0406a
b0tguy <117023176+b0tguy@users.noreply.github.com>
2026-07-18 15:11:56 +0200

    Update config.js

--- src/static/scripts/config.js
+++ src/static/scripts/config.js
@@ -1,6 +1,22 @@
+const backendOrigin = (
+  import.meta.env.VITE_PROXY_ORIGIN || ""
+).replace(/\/$/, "");
+
+function toWebSocketOrigin(origin) {
+  return origin
+    .replace(/^https:/i, "wss:")
+    .replace(/^http:/i, "ws:");
+}
+
 export const CONFIG = {
-  bUrl: '/seal/',
-  ws: '/wisp/',
-  unsupported: ['spotify.com'],
-  filter: ['neal.fun'],
+  bUrl: backendOrigin
+    ? `${backendOrigin}/seal/`
+    : "/seal/",
+
+  ws: backendOrigin
+    ? `${toWebSocketOrigin(backendOrigin)}/wisp/`
+    : "/wisp/",
+
+  unsupported: ["spotify.com"],
+  filter: ["neal.fun"],
 };
